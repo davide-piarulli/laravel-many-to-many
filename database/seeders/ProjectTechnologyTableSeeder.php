1e325fe7cbb53a102df359ce9b269489feaa02cf
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-29 15:01:19 +0100

    push ticket

--- database/seeders/ProjectTechnologyTableSeeder.php
+++ database/seeders/ProjectTechnologyTableSeeder.php
@@ -20,7 +20,7 @@ class ProjectTechnologyTableSeeder extends Seeder
             $project = Project::inRandomOrder()->first();
 
             // estraggo l'ID della technology random
-            $technology_id = Technology::inRandomOrder()->first()->id;
+            $technology_id = Technology::inRandomOrder()->first()->technology_id;
 
             // aggiungo relazione tra project e technologies
             $project->technologies()->attach($technology_id);
